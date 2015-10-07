a3dabcf9fd9f68c300714443aad22e2e942184f7
root <root@default.hostname>
2015-10-07 07:51:56 -0400

    first commit

---
+++ ex39_test.py
@@ -0,0 +1,17 @@
+import hashmap
+
+# create a mapping of state to avvreviatuion
+states = hashmap.new()
+hashmap.set(states, 'Orengon', 'OR')
+hashmap.set(states, 'Florida', 'FL')
+hashmap.set(states, 'California', 'CA')
+hashmap.set(states, 'New York', 'NY')
+hashmap.set(states, 'Michigan', 'MI')
+
+#create a mapping of states and some cities in them
+cities = hashmap.new()
+hashmap.set(states, 'Michigan', 'MI')
+
+hashmap.set(states, 'Michigan', 'MI')
+hashmap.set(states, 'Michigan', 'MI')
+hashmap.set
---
+++ test.py
@@ -0,0 +1,9 @@
+states = {
+    'Oregon': 'OR',
+	'Florida': 'FL',
+	'California': 'CA',
+    'New York': 'NY',
+    'Michigan': 'MI'
+}
+for idx, val in states.items():
+	print idx, val
